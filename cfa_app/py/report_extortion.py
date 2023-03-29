89495817138580be646763d0badb8d5526b66657
ahmed-fawzy <ahmed.linuxawy@gmail.com>
2023-03-29 13:43:22 +0200

    add select options dialog, test camera

--- cfa_app/py/report_extortion.py
+++ cfa_app/py/report_extortion.py
@@ -1,24 +1,43 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelOneLine
 from kivy.uix.modalview import ModalView
 from kivymd.uix.snackbar import Snackbar
-
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.list import OneLineIconListItem
 
 from kivy_garden.mapview import MapView, MapMarker
 
+class Item(OneLineIconListItem):
+    divider = None
+    source = StringProperty()
 
 class Video_Photo_Content(MDBoxLayout):
 
     camera_popup = None
 
-    def capture_photo(self):
+    def capture_img(self, instance):
         self.camera_popup = Camera_Popup()
         self.camera_popup.parent_instance = self
         self.camera_popup.open()    
 
+
+    def choose_option_photo(self):
+        self.dialog = MDDialog(
+            title="Select Option",
+            type="simple",
+            items=[
+                Item(text="Gallery", source="folder-multiple-image", on_release=self.choose_img_gallery),
+                Item(text="Camera", source="camera", on_release=self.capture_img),
+            ],
+        )
+        self.dialog.open()
+
+
+    def choose_img_gallery(self, instance):
+        pass
 
 
 class Location_Content(MDBoxLayout):
@@ -63,9 +82,10 @@ class Camera_Popup(ModalView):
 
         # dismiss popup
         self.dismiss()
-        print(self.parent)
         self.parent_instance.ids.filename_box.text = filename
 
+        #destroy instance
+        self.__del__()
 
 
 

6a907024301ff830e99a3adfaf1fc42a80657124
Shantam <shantam.shrestha@gmail.com>
2023-07-08 21:27:35 +0545

    settings and requirements updated

--- cfa_server/cfa_server/settings.py
+++ cfa_server/cfa_server/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
-    'rest_framework.authtoken',   
+    'rest_framework.authtoken',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [

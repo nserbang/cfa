fed6ccfdd5bc0ed7cbdd96ae9ce15eee8bfd8987
Shantam <shantam.shrestha@gmail.com>
2023-07-11 00:21:59 +0530

    update

--- cfa_server/api/serializers.py
+++ cfa_server/api/serializers.py
@@ -10,7 +10,7 @@ class DistrictSerializer(serializers.ModelSerializer):
 class cUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = cUser
-        fields = '__all__'
+        fields = ['id','email','first_name','last_name']
 
 
 class PoliceStationContactSerializer(serializers.ModelSerializer):
@@ -20,13 +20,17 @@ class PoliceStationContactSerializer(serializers.ModelSerializer):
 
 
 class PoliceStationSerializer(serializers.ModelSerializer):
-   # contact = PoliceStationSerializer(many=True)
+
+    district = DistrictSerializer(source='did')
+
     class Meta:
         model = PoliceStation
         fields = '__all__'
 
-
 class PoliceOfficerSerializer(serializers.ModelSerializer):
+
+    police_station = PoliceStationSerializer(source='pid')
+
     class Meta:
         model = PoliceOfficer
         fields = '__all__'
@@ -36,7 +40,6 @@ class CaseHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = CaseHistory
         fields = '__all__'
-
 
 class MediaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -48,10 +51,26 @@ class CaseSerializer(serializers.ModelSerializer):
     #history = CaseHistorySerializer(many=True)
     #media = MediaSerializer(many=True)
 
+    case_type = serializers.SerializerMethodField()
+    case_state = serializers.SerializerMethodField()
+    # police_station = serializers.PrimaryKeyRelatedField(queryset=PoliceStation.objects.all())
+    police_officer = PoliceOfficerSerializer(source='oid')
+    comment_count = serializers.SerializerMethodField()
+    user_detail = cUserSerializer(source='user')
+
+
     class Meta:
         model = Case
-        fields = '__all__'
+        fields = ['cid','police_officer', 'user_detail', 'case_type', 'title', 'case_state', 'created', 'lat', 'long', 'description', 'follow','comment_count']
 
+    def get_case_type(self, case):
+        return dict(Case.cType)[case.type]
+
+    def get_case_state(self, case):
+        return dict(Case.cState)[case.cstate]
+
+    def get_comment_count(self, case):
+        return case.comment_set.count()
 
 class LostVehicleSerializer(serializers.ModelSerializer):
     class Meta:

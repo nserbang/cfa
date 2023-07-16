d6ceb9c2ef1b53500b210d6860b5eeee91cc598a
Shantam <shantam.shrestha@gmail.com>
2023-07-16 08:21:55 +0530

    added filter for django

--- cfa_server/api/viewset/information.py
+++ cfa_server/api/viewset/information.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from api.models import Information
 from api.serializers import InformationSerializer
@@ -7,6 +9,10 @@ from api.viewset.permission import IsAuthenticatedOrWriteOnly
 class InformationViewSet(ModelViewSet):
     serializer_class = InformationSerializer
     queryset = Information.objects.all()
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['inid', 'information_type']
+    search_fields = ['information_type', 'intro']
+    ordering_fields = ['information_type', 'inid']
 
     def get_permissions(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
@@ -14,3 +20,6 @@ class InformationViewSet(ModelViewSet):
         else:
             permission_classes = []
         return [permission() for permission in permission_classes]
+    
+    def get_queryset(self):
+        return super().get_queryset()
--- cfa_server/cfa_server/settings.py
+++ cfa_server/cfa_server/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'rest_framework.authtoken',
-    'drf_yasg'
+    'drf_yasg',
+    'django_filters'
 ]
 
 MIDDLEWARE = [

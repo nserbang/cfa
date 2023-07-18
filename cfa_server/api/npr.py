2df824a0d3849b606decce6198a96f38f2411f91
nserbang <nserbang@gmail.com>
2023-07-18 07:55:42 +0530

    Adding vehicle detection logic

--- cfa_server/api/npr.py
+++ cfa_server/api/npr.py
@@ -1,19 +1,26 @@
 import cv2 as cv
 import numpy as np
 import pytesseract as pt
-
+from models import LostVehicle
 # INPUT: Image or vehicle numbers
 # OUTPUT: Vehicle number annotated input image, list of vehicle numbers detected in the image
 
 def detectVehicleNumber(img = None, numbers = None):  
     #cv.imshow(" Found :",img)
-
+    ret_nums = []
     #cv.waitKey(0)
     if numbers is not None:
         # TO DO: search num in lostVehicle. If found, return as it is
         # fetch numbers from the lostVehicle table and return it
-        found = None
-        return None, found
+        found  = LostVehicle.objects.get(regNumber = numbers)
+        if found is None: # it is possible that numbers was chasis number
+            found = LostVehicle.objects.get(chasisNumber = numbers)
+        if found is None: # it is possible that numbers was chasis number
+            found = LostVehicle.objects.get(engineNumber = numbers)            
+        if found is not None:
+            ret_nums.append({numbers:True})
+        else:
+            ret_nums.append({numbers:False})    
     if img is not None:
         car = img
         cascade = cv.CascadeClassifier('haarcascade_number_plate.xml')
@@ -28,13 +35,13 @@ def detectVehicleNumber(img = None, numbers = None):
             cv.rectangle(car, (x,y), (x+w,y+h), (0,0,255), 5) # Draw rectangle around the number plate in the original image
             npr.append(car[y+20:y+h-10 ,x+20:x+w-15]) # strip the number plate
         # TO DO : Change tesseract_cmd for linux as required #
-        pt.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+        pt.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
         custom_config = r'--oem 3 --psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     
         for n in npr:
             width = int(n.shape[1]*150/100) 
             height = int(n.shape[0]*150/100)
-            print(" HELLO width :",width, "Height :",height, " Shape :",n.shape[0])
+            #print(" HELLO width :",width, "Height :",height, " Shape :",n.shape[0])
             dm = (width,height) 
             #print(" NNNN : ",n)
             carTemp = cv.resize(n,dm,interpolation=cv.INTER_AREA) # resize image for OCR
@@ -44,19 +51,14 @@ def detectVehicleNumber(img = None, numbers = None):
             grayBlur = cv.medianBlur(grayLarged,3)
 
             num = pt.image_to_string(grayBlur, config=custom_config)
-            print (' NUMBER : ',num)
+            rcn = LostVehicle.objects.get(regNumber = num)
+            if rcn is not None:
+                ret_nums.append({rcn:True})
+            else:
+                ret_nums.append({rcn:False})
+            print (' NUMBER  in image : ',num)
             nums.append(num) # get recognized numbers
 
-        #dm = (size,size)
-        #car = cv.resize(car,dm,interpolation=cv.INTER_AREA)
-        ret_nums = []
-        for num in nums:
-            # for each num, check this value in LostVehicle table
-            # TO DO: search num in lostVehicle. If found, return as it is 
-            found = None # fill this with value returned from lostVehicle table
-            if found is not None:
-                ret_nums.append(found)
-            return None,found  # image is null
         return car, ret_nums
 
 
@@ -64,7 +66,7 @@ def detectVehicleNumber(img = None, numbers = None):
 #cp = cv.VideoCapture(0)
 def test () :
     img =  cv.imread("cars.jpg")
-    img, nums= detectVehicleNumber(img)
+    img, nums= detectVehicleNumber(img,"1234")
 
     for i in nums:
         print(" Vehicle Number found ",i)
@@ -76,3 +78,4 @@ def test () :
     cv.destroyAllWindows()
 
 #test()
+

75be12227f090836d231888e0686e3c1e225b59c
Subodh <subodh.ur@gmail.com>
2023-07-23 17:47:26 +0545

    auto latitude and longitue from browser while adding case

--- cfa_server/api/forms/case.py
+++ cfa_server/api/forms/case.py
@@ -8,36 +8,35 @@ class CaseForm(forms.ModelForm):
     class Meta:
         model = Case
         fields = [
-            # 'pid',
-            # 'user',
-            # 'oid',
-            'type',
-            'title',
-            'cstate',
-            'lat',
-            'long',
-            'description',
-            'follow',
+            "type",
+            "title",
+            "cstate",
+            "lat",
+            "long",
+            "description",
+            "follow",
         ]
 
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user')
+        self.user = kwargs.pop("user")
         super().__init__(*args, **kwargs)
-        self.fields['cstate'].label = 'State'
-        self.fields['lat'].label = 'Latitude'
-        self.fields['long'].label = 'Longitude'
+        self.fields["cstate"].label = "State"
+        self.fields["lat"].widget = forms.widgets.HiddenInput()
+        self.fields["long"].widget = forms.widgets.HiddenInput()
 
     def save(self, commit=False):
         case = super().save(commit=False)
         geo_location = fromstr(f"POINT({case.lat} {case.long})", srid=4326)
         user_distance = Distance("geo_location", geo_location)
-        police_station = PoliceStation.objects.annotate(
-            radius=user_distance
-        ).order_by('radius').first()
+        police_station = (
+            PoliceStation.objects.annotate(radius=user_distance)
+            .order_by("radius")
+            .first()
+        )
         # import pdb; pdb.set_trace()
         case.pid = police_station
         case.geo_location = geo_location
-        officier = police_station.policeofficer_set.order_by('-rank').first()
+        officier = police_station.policeofficer_set.order_by("-rank").first()
         case.oid = officier
         case.user = self.user
         return case.save()

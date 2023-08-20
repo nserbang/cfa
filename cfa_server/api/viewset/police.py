e228195f314045b8c3c2013d0a8b6b9cf651f8e9
Subodh <subodh.ur@gmail.com>
2023-08-20 11:45:43 +0545

    other police officer list from the police station of the transferring police officer

--- cfa_server/api/viewset/police.py
+++ cfa_server/api/viewset/police.py
@@ -1,29 +1,30 @@
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from api.viewset.permission import IsPoliceOfficer
 
-from api.models import (
-    PoliceStation,
-    PoliceStationContact,
-    PoliceOfficer
-)
+from api.models import PoliceStation, PoliceStationContact, PoliceOfficer
 
 from api.serializers import (
     PoliceStationSerializer,
     PoliceStationContactSerializer,
-    PoliceOfficerSerializer
+    PoliceOfficerSerializer,
 )
+
 
 class PoliceStationViewSet(ModelViewSet):
     serializer_class = PoliceStationSerializer
     queryset = PoliceStation.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
+
 # View cor Police Station Contact
 class PoliceStationContactViewSet(ModelViewSet):
-
     serializer_class = PoliceStationContactSerializer
     queryset = PoliceStationContact.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,)
+
 
 # Views for Police Officers
 class PoliceOfficerViewSet(ModelViewSet):
@@ -31,12 +32,21 @@ class PoliceOfficerViewSet(ModelViewSet):
     queryset = PoliceOfficer.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
-class DistrictPoliceStationViewSet(ReadOnlyModelViewSet):
+    @action(detail=False, methods=["GET"], permission_classes=[IsPoliceOfficer])
+    def others(self, request):
+        current_officer = request.user.policeofficer_set.first()
+        officers = PoliceOfficer.objects.filter(pid=current_officer.pid)
+        serializer = PoliceOfficerSerializer(
+            officers, many=True, context={"request": request}
+        )
+        return Response(status=200, data=serializer.data)
 
+
+class DistrictPoliceStationViewSet(ReadOnlyModelViewSet):
     serializer_class = PoliceStationSerializer
     queryset = PoliceStation.objects.all()
 
     def get_queryset(self):
-        district_id = self.kwargs['district_id']
+        district_id = self.kwargs["district_id"]
         queryset = PoliceStation.objects.filter(did=district_id)
         return queryset

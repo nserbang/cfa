766c63ff2e9f1302a7510c29438a3a7861f6d6c6
shantamshrestha <shantamshrestha@gmail.com>
2023-11-18 12:33:35 +0545

    update gunicorn config

--- cfa_server/gunicorn_config.py
+++ cfa_server/gunicorn_config.py
@@ -1,4 +1,4 @@
 import multiprocessing
 
-bind = "0.0.0.0:9005"  # The IP and port Gunicorn will listen on
+bind = "127.0.0.1:9005"  # The IP and port Gunicorn will listen on
 workers = multiprocessing.cpu_count() * 2 + 1  # Number of worker processes

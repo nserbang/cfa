9bfe51c590cd5d3c88ec651cd941b38dac65eb2a
shantamshrestha <shantamshrestha@gmail.com>
2023-12-12 21:38:33 +0545

    settings updated

--- cfa_server/cfa_server/settings.py
+++ cfa_server/cfa_server/settings.py
@@ -393,7 +393,7 @@ CSP_SCRIPT_HASHES = (
     "'sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3'",
 )
 CSP_FONT_SRC = ("'self'",)
-CSP_STYLE_SRC = ("'self'", "www.google.com")
+CSP_STYLE_SRC = ("'self'", "'nonce'", "www.google.com")
 CSP_OBJECT_SRC = "'none'"
 CSP_STYLE_SRC_ELEM = ("'self'", "www.google.com")
 CSP_INCLUDE_NONCE_IN = ('script-src', 'style-src-elem', 'img-src data', 'style-src')
